eb9bc55397dac261d585da45ebba814de5d427b0
priyaranjan singh <58334735+priyeranjansingh@users.noreply.github.com>
2020-02-24 03:10:55 -0800

    Create page.php

---
+++ page.php
@@ -0,0 +1,6 @@
+/*
+* open wp-config.php file and copy paste the below code
+* this will solve the issue file type not supported or not permitted for security reasons
+*/
+
+define('ALLOW_UNFILTERED_UPLOADS', true);
